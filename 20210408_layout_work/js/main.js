ce022b7bf8eb5e47983386628605e645127358bc
katsukawagami <joelai1988@gmail.com>
2021-04-14 00:25:16 +0800

    function this 問題解決

--- 20210408_layout_work/js/main.js
+++ 20210408_layout_work/js/main.js
@@ -1,14 +1,14 @@
-function oneMinus() {
+function oneMinus(element) {
     // let nameValue = this.getAttribute('name').value;
     // this.value;
-    console.log(this.attr);
-    let gotValue = document.querySelector(`input[name=${nameValue}]`).value;
-    if (gotValue == '') {
-        gotValue = 0;
-    }
-    console.log(gotValue);
-    gotValue--;
-    console.log(gotValue);
+    console.log(element.name);
+    // let gotValue = document.querySelector(`input[name=${nameValue}]`).value;
+    // if (gotValue == '') {
+    //     gotValue = 0;
+    // }
+    // console.log(gotValue);
+    // gotValue--;
+    // console.log(gotValue);
 }
 
 function onePlus() {
