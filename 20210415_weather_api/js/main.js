4aa85fd60ea0cc3b87bae4cc95ef83e9ef3ae3e5
katsukawagami <joelai1988@gmail.com>
2021-04-15 19:48:20 +0800

    取得資料render頁面完成

--- 20210415_weather_api/js/main.js
+++ 20210415_weather_api/js/main.js
@@ -1,13 +1,19 @@
 
-
 fetch('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-24A1604D-58DB-4133-A411-7CBE956E3B4B')
     .then(function (response) {
         return response.text();
     })
     .then(function (result) {
-        let resultJson = JSON.parse(result);
-        console.log(resultJson.records.location);
-    })
+        let resultJson = JSON.parse(result).records.location;
+
+        const vm = Vue.createApp({
+            data() {
+                return {
+                    resultJson
+                }
+            }
+        }).mount('#app');
+    });
 
 
 
@@ -24,21 +30,5 @@ fetch('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorizati
 
 
 
-// const vm = Vue.createApp({
-//     data() {
-//         return {
 
-//             isShow:true
-
-//             // arr: [
-//             //     { title: '品項1', hashId: '_' + Math.floor(Math.random() * 1000000) },
-//             //     { title: '品項2', hashId: '_' + Math.floor(Math.random() * 1000000) },
-//             //     { title: '品項3', hashId: '_' + Math.floor(Math.random() * 1000000) },
-//             //     { title: '品項4', hashId: '_' + Math.floor(Math.random() * 1000000) },
-//             //     { title: '品項5', hashId: '_' + Math.floor(Math.random() * 1000000) }
-//             // ]
-
-//         }
-//     }
-// }).mount('#app');
 
