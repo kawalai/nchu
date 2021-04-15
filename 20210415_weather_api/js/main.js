15af4679c634f9665c6f5de6d11522c9647580c3
kawagami <joelai1988@gmail.com>
2021-04-15 16:43:11 +0800

    vue installed

--- 20210415_weather_api/js/main.js
+++ 20210415_weather_api/js/main.js
@@ -1,2 +1,17 @@
-// todo
+const vm = Vue.createApp({
+    data() {
+        return {
 
+            isShow:true
+
+            // arr: [
+            //     { title: '品項1', hashId: '_' + Math.floor(Math.random() * 1000000) },
+            //     { title: '品項2', hashId: '_' + Math.floor(Math.random() * 1000000) },
+            //     { title: '品項3', hashId: '_' + Math.floor(Math.random() * 1000000) },
+            //     { title: '品項4', hashId: '_' + Math.floor(Math.random() * 1000000) },
+            //     { title: '品項5', hashId: '_' + Math.floor(Math.random() * 1000000) }
+            // ]
+
+        }
+    }
+}).mount('#app');
