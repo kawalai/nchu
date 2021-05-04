df81f23f3398ec64426d021c7c23f85ee278d866
kawagami <joelai1988@gmail.com>
2021-05-04 15:13:41 +0800

    note update

--- laravel7/routes/web.php
+++ laravel7/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    $data = '123';
+    return view('test', ['data' => $data]);
+});
