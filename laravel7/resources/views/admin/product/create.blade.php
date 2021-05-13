560bd3b85f77fc16a2b55b421ab00f17d5b2fe0d
kawagami <joelai1988@gmail.com>
2021-05-13 15:08:59 +0800

    style update

--- laravel7/resources/views/admin/product/create.blade.php
+++ laravel7/resources/views/admin/product/create.blade.php
@@ -9,30 +9,30 @@
 <div class="container">
     <form action="{{route('store')}}" method="POST" enctype="multipart/form-data">
         @csrf
-        <div class="form-group">
-            <label for="">類型</label>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="">類型</label>
             {{-- <input name="type_id" type="text" /> --}}
-            <select name="type_id" id="type_id">
+            <select class="form-control" name="type_id" id="type_id">
                 @foreach ($productTypes as $productType)
                 <option value="{{$productType->id}}">{{$productType->name}}</option>
                 @endforeach
             </select>
         </div>
-        <div class="form-group">
-            <label for="name">名稱</label>
-            <input id="name" name="name" type="text" />
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="name">名稱</label>
+            <input class="form-control" id="name" name="name" type="text" />
         </div>
-        <div class="form-group">
-            <label for="price">價格</label>
-            <input id="price" name="price" type="text" />
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="price">價格</label>
+            <input class="form-control" id="price" name="price" type="text" />
         </div>
-        <div class="form-group">
-            <label for="img">圖片</label>
-            <input id="img" name="img" type="file" accept="image/*" />
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="img">圖片</label>
+            <input class="form-control" id="img" name="img" type="file" accept="image/*" />
         </div>
-        <div class="form-group">
-            <label for="description">敘述</label>
-            <textarea id="description" name="description" cols="30" rows="10"></textarea>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="description">敘述</label>
+            <textarea class="form-control" id="description" name="description" cols="30" rows="10"></textarea>
         </div>
         <button type="submit" class="btn btn-primary">新增</button>
     </form>
--- laravel7/resources/views/admin/product/edit.blade.php
+++ laravel7/resources/views/admin/product/edit.blade.php
@@ -9,30 +9,30 @@
 <div class="container">
     <form action="{{route('update', ['id'=>$data->id])}}" method="post" enctype="multipart/form-data">
         @csrf
-        <div class="form-group">
-            <label for="type_id">類型</label>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="type_id">類型</label>
             {{-- <input name="type_id" type="text" value="{{$data->type}}" /> --}}
-            <select name="type_id" id="type_id">
+            <select class="form-control" name="type_id" id="type_id">
                 @foreach ($productTypes as $productType)
                 <option class="form-control" value="{{$productType->id}}" @if ($data->type_id == $productType->id) selected @endif>{{$productType->name}}</option>
                 @endforeach
             </select>
         </div>
-        <div class="form-group">
-            <label for="name">名稱</label>
-            <input id="name" name="name" type="text" value="{{$data->name}}" />
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="name">名稱</label>
+            <input class="form-control" id="name" name="name" type="text" value="{{$data->name}}" />
         </div>
-        <div class="form-group">
-            <label for="price">價格</label>
-            <input id="price" name="price" type="text" value="{{$data->price}}" />
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="price">價格</label>
+            <input class="form-control" id="price" name="price" type="text" value="{{$data->price}}" />
         </div>
-        <div class="form-group">
-            <label for="img">圖片</label>
-            <input id="img" name="img" type="file" accept="image/*" value="{{$data->img}}" />
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="img">圖片</label>
+            <input class="form-control" id="img" name="img" type="file" accept="image/*" value="{{$data->img}}" />
         </div>
-        <div class="form-group">
-            <label for="description">敘述</label>
-            <textarea id="description" name="description" cols="30" rows="10">{{$data->description}}</textarea>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label" for="description">敘述</label>
+            <textarea class="form-control" id="description" name="description" cols="30" rows="10">{{$data->description}}</textarea>
         </div>
         <button type="submit" class="btn btn-primary">修改</button>
     </form>
